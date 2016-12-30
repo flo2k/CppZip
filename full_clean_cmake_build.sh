c52a58b347e43ec1c340379eca48b6c794a94f67
Florian Künzner <flo.kuenzner@gmx.net>
2016-12-30 14:36:25 +0100

    cmake build (comments)

--- full_clean_cmake_build.sh
+++ full_clean_cmake_build.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
+##prepare build dir
+#deletes the build dir if exists
 [ -d build ] && rm -Rf build
+#creates the build dir
 mkdir build
 
+##prepare build
+# for options details see: http://stackoverflow.com/questions/31090821/what-does-the-h-option-means-for-cmake
+# -H set source dir
+# -B set binary dir
 cmake -H. -Bbuild
+
+##start build
+# --build set build dir
+# -j parallel build
 cmake --build build -- -j
+
+#show files in build dir
 ls -al build
 
 #start tests
