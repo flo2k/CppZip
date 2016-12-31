448d23af26f53b99172c8806c19249859c9101a0
Florian Künzner <flo.kuenzner@gmx.net>
2016-12-31 18:15:26 +0100

    windows build: dll

---
+++ src/CppZip/ZipCommon.cpp
@@ -0,0 +1,21 @@
+#include "ZipCommon.h"
+
+namespace cppzip {
+
+InnerZipFileInfo::InnerZipFileInfo(void)
+{
+	time_sec = 0;
+	time_min = 0;
+	time_hour = 0;
+	time_day_of_month = 0;
+	time_month = 0;
+	time_year = 0;
+	dosDate = 0;
+	crc = 0;
+	compressed_size = 0;
+	uncompressed_size = 0;
+	internal_fileAttributes = 0;
+	external_fileAttributes = 0;
+}
+
+} //cppzip
--- src/CppZip/ZipCommon.h
+++ src/CppZip/ZipCommon.h
@@ -51,20 +51,7 @@ namespace cppzip {
  */
 struct CPPZIP_SHARED_EXPORT InnerZipFileInfo
 {
-	InnerZipFileInfo(void){
-		time_sec = 0;
-		time_min = 0;
-		time_hour = 0;
-		time_day_of_month = 0;
-		time_month = 0;
-		time_year = 0;
-		dosDate = 0;
-		crc = 0;
-		compressed_size = 0;
-		uncompressed_size = 0;
-		internal_fileAttributes = 0;
-		external_fileAttributes = 0;
-	}
+	InnerZipFileInfo(void);
 
 	std::string fileName;
 	std::string extraField;
