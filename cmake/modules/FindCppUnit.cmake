f55544a0d4349d76b75fbdcfef751ae89c6e662b
Florian Künzner <flo.kuenzner@gmx.net>
2016-12-31 15:09:21 +0100

    build and run tests

--- cmake/modules/FindCppUnit.cmake
+++ cmake/modules/FindCppUnit.cmake
@@ -1,35 +1,76 @@
-# 2011-05-28 
-# Copied from http://root.cern.ch/viewvc/trunk/cint/reflex/cmake/modules/FindCppUnit.cmake
-
-# - Find CppUnit
-# This module finds an installed CppUnit package.
+## FindCppUnit.cmake copied from https://github.com/rpavlik/cmake-modules/blob/master/Findcppunit.cmake
+# - try to find cppunit library
 #
-# It sets the following variables:
-#  CPPUNIT_FOUND       - Set to false, or undefined, if CppUnit isn't found.
-#  CPPUNIT_INCLUDE_DIR - The CppUnit include directory.
-#  CPPUNIT_LIBRARY     - The CppUnit library to link against.
+# Cache Variables: (probably not for direct use in your scripts)
+#  CPPUNIT_INCLUDE_DIR
+#  CPPUNIT_LIBRARY
+#
+# Non-cache variables you might use in your CMakeLists.txt:
+#  CPPUNIT_FOUND
+#  CPPUNIT_INCLUDE_DIRS
+#  CPPUNIT_LIBRARIES
+#
+# Requires these CMake modules:
+#  SelectLibraryConfigurations (included with CMake >= 2.8.0)
+#  FindPackageHandleStandardArgs (known included with CMake >=2.6.2)
+#
+# Original Author:
+# 2009-2011 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+# http://academic.cleardefinition.com
+# Iowa State University HCI Graduate Program/VRAC
+#
+# Copyright Iowa State University 2009-2011.
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
 
-FIND_PATH(CPPUNIT_INCLUDE_DIR cppunit/Test.h)
-FIND_LIBRARY(XX NAMES cppunit.lib ${CPPUNIT_LIBRARY} ${${search}} PATH_SUFFIXES lib)
-MESSAGE(STATUS "XX: " ${XX})
-SET(CPPUNIT_LIBRARY ${XX})
+set(CPPUNIT_ROOT_DIR
+	"${CPPUNIT_ROOT_DIR}"
+	CACHE
+	PATH
+	"Directory to search")
 
-IF (CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
-   SET(CPPUNIT_FOUND TRUE)
-ENDIF (CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
+find_library(CPPUNIT_LIBRARY_RELEASE
+	NAMES
+	cppunit
+	HINTS
+	"${CPPUNIT_ROOT_DIR}")
 
-IF (CPPUNIT_FOUND)
+find_library(CPPUNIT_LIBRARY_DEBUG
+	NAMES
+	cppunitd
+	HINTS
+	"${CPPUNIT_ROOT_DIR}")
 
-   # show which CppUnit was found only if not quiet
-   IF (NOT CppUnit_FIND_QUIETLY)
-      MESSAGE(STATUS "Found CppUnit: ${CPPUNIT_LIBRARY}")
-   ENDIF (NOT CppUnit_FIND_QUIETLY)
+include(SelectLibraryConfigurations)
+select_library_configurations(CPPUNIT)
 
-ELSE (CPPUNIT_FOUND)
+# Might want to look close to the library first for the includes.
+get_filename_component(_libdir "${CPPUNIT_LIBRARY_RELEASE}" PATH)
 
-   # fatal error if CppUnit is required but not found
-   IF (CppUnit_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find CppUnit")
-   ENDIF (CppUnit_FIND_REQUIRED)
+find_path(CPPUNIT_INCLUDE_DIR
+	NAMES
+	cppunit/TestCase.h
+	HINTS
+	"${_libdir}/.."
+	PATHS
+	"${CPPUNIT_ROOT_DIR}"
+	PATH_SUFFIXES
+	include/)
 
-ENDIF (CPPUNIT_FOUND)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(cppunit
+	DEFAULT_MSG
+	CPPUNIT_LIBRARY
+	CPPUNIT_INCLUDE_DIR)
+
+if(CPPUNIT_FOUND)
+	set(CPPUNIT_LIBRARIES ${CPPUNIT_LIBRARY} ${CMAKE_DL_LIBS})
+	set(CPPUNIT_INCLUDE_DIRS "${CPPUNIT_INCLUDE_DIR}")
+	mark_as_advanced(CPPUNIT_ROOT_DIR)
+endif()
+
+mark_as_advanced(CPPUNIT_INCLUDE_DIR
+	CPPUNIT_LIBRARY_RELEASE
+CPPUNIT_LIBRARY_DEBUG)
