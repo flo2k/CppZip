c7c1f0d421801d1c072800c2b63caf5185420e53
Florian Künzner <flo.kuenzner@gmx.net>
2017-12-01 20:05:41 +0100

    enable verbose build for debugging on AppVeyor

--- full_clean_cmake_build.sh
+++ full_clean_cmake_build.sh
@@ -10,13 +10,13 @@ mkdir build
 # for options details see: http://stackoverflow.com/questions/31090821/what-does-the-h-option-means-for-cmake
 # -H set source dir
 # -B set binary dir
-cmake -H. -Bbuild -DBUILD_TESTS=1
+cmake -H. -Bbuild -DBUILD_TESTS=1 -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
 
 ##start build
 # --build set build dir
 # -j parallel build
 cmake --build build -- -j
-cmake --build build -- -j doc
+#cmake --build build -- -j doc
 
 #or
 #make -C build -j all doc
